7611c7ee0e022ea19297f1db3f4fa9e39fdc1c3b
Kayla Kingston <kkingston09@gmail.com>
2020-05-27 11:52:39 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,7 +2,7 @@
 #   puts "Hi, my name is #{name}."
 # end
 
-def introduction(name, language)
+def introduction_wiht_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
